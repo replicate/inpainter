38c19edf99541417f08b7dd1442ed28b7c989b8f
Zeke Sikelianos <zeke@sikelianos.com>
2022-09-06 23:08:11 -0700

    update home styles

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="max-w-[512px] mx-auto my-10 p-10 bg-white rounded-lg">
       <h1 className="text-center text-7xl pb-3">🎨</h1>
-      <p className="pb-5">
+      <p className="py-5">
         <strong>Inpainting</strong> is a process where missing parts of an
         artwork are filled in to present a complete image. This demo uses the{" "}
         <a
@@ -13,19 +13,24 @@ export default function About() {
         >
           Stable Diffusion
         </a>{" "}
-        machine learning model to inpaint images.
+        machine learning model and the{" "}
+        <a className="underline" href="https://replicate.com/docs">
+          Replicate API
+        </a>{" "}
+        to inpaint images right in your browser.
       </p>
 
       <ol className="list-decimal pl-5">
         <li className="mb-2">
-          Enter a text prompt below and hit &quot;Generate&quot;.
+          Enter a text prompt to generate an image, or upload your own starting
+          image.
         </li>
         <li className="mb-2">
-          After a few seconds, you&apos;ll see a generated image. Click and drag
-          to erase unwanted parts of the image.
+          Click and drag with your mouse to erase unwanted parts of the image.
         </li>
         <li className="mb-2">
-          Refine your prompt (or leave it) and hit &quot;Generate&quot; again
+          Refine your text prompt (or leave it untouched) and let the model
+          generate a new inpainted image.
         </li>
       </ol>
 

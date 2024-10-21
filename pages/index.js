37f03d6bf25e1280d64fb5420a3a958a04636da5
Zeke Sikelianos <zeke@sikelianos.com>
2024-10-21 14:05:47 -0700

    remove dead code

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ export default function About() {
         <title>Inpainting with Stable Diffusion &amp; Replicate</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {/* <h1 className="text-center text-7xl pb-3">🎨</h1> */}
       <p className="pb-5 text-lg">
         <strong>Inpainting</strong> is a process where missing parts of an
         artwork are filled in to present a complete image. This{" "}
@@ -34,20 +33,6 @@ export default function About() {
           <source src="/cherries-oranges-bananas.mp4" />
         </video>
       </Link>
-
-      {/* <ol className="list-decimal pl-5">
-        <li className="mb-2">
-          Enter a text prompt to generate an image, or upload your own starting
-          image.
-        </li>
-        <li className="mb-2">
-          Click and drag with your mouse to erase unwanted parts of the image.
-        </li>
-        <li className="mb-2">
-          Refine your text prompt (or leave it untouched) and let the model
-          generate a new inpainted image.
-        </li>
-      </ol> */}
 
       <Link href="/paint">
         <a className="py-3 block text-center bg-black text-white rounded-md mt-10">

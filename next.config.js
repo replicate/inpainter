a3d0bc825651d00e094df15ce9127d6a10e31514
Matt Rothenberg <mattrothenberg@gmail.com>
2025-04-14 09:50:11 -0400

    chore: disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "replicate.com",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.delivery",
-      },
-      {
-        protocol: "https",
-        hostname: "*.replicate.delivery",
-      },
-    ],
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true,
+	},
 };
 
 module.exports = nextConfig;
